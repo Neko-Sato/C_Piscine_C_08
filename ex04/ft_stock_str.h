acf113de9d1f3a05da6d3b0e871b11ed7b8707f6
hshimizu <hisatosi35350@gmail.com>
2023-03-21 23:40:41 +0900

    とりあえずプッシュ

--- ex04/ft_stock_str.h
+++ ex04/ft_stock_str.h
@@ -1,29 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*   ft_stock_str.h                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/21 23:25:52 by hshimizu          #+#    #+#             */
-/*   Updated: 2023/03/21 23:39:06 by hshimizu         ###   ########.fr       */
+/*   Created: 2023/03/20 16:17:51 by hshimizu          #+#    #+#             */
+/*   Updated: 2023/03/20 16:53:40 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_BOOLEAN_H
-# define FT_BOOLEAN_H
+#ifndef FT_STOCK_H
+# define FT_STOCK_H
 
-# include <unistd.h>
-
-# define TRUE 1
-# define FALSE 0
-# define SUCCESS 0
-
-# define EVEN_MSG "I have an even number of arguments.\n"
-# define ODD_MSG "I have an odd number of arguments.\n"
-
-# define EVEN(x) (x % 2)
-
-typedef char	t_bool;
+typedef struct s_stock_str
+{
+	int		size;
+	char	*str;
+	char	*copy;
+}			t_stock_str;
 
 #endif
