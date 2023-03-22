846234eeda4c876448256efcefcfe525f18889dc
hshimizu <hisatosi35350@gmail.com>
2023-03-22 18:34:03 +0900

    ミスった

--- ex01/ft_boolean.h
+++ ex01/ft_boolean.h
@@ -6,7 +6,7 @@
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 23:25:52 by hshimizu          #+#    #+#             */
-/*   Updated: 2023/03/21 23:39:06 by hshimizu         ###   ########.fr       */
+/*   Updated: 2023/03/22 18:33:32 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,8 @@
 # define EVEN_MSG "I have an even number of arguments.\n"
 # define ODD_MSG "I have an odd number of arguments.\n"
 
-# define EVEN(x) (x % 2)
+# define ODD(x) (x % 2)
+# define EVEN(x) (!ODD(x))
 
 typedef char	t_bool;
 
